2fea1a19e7420e64074115da2e5ba7b1b3c515d9
Felipe Pinto <felipepinto.fpm@gmail.com>
2022-05-17 12:36:29 -0300

    Frist commit skynews

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>sky.news</title>
+      </Head>
+
+      <h1>Hellow Dev</h1>
+    </>
+  )
+}
