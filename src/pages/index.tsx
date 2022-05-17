d3e2bfca801ff78a0f35cf37141dbd2e22b87fe2
Felipe Pinto <felipepinto.fpm@gmail.com>
2022-05-17 15:50:22 -0300

    Create home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head'
 
+import styles from '../styles/stylesPages/home.module.scss'
+import { SubscribButton } from './../components/SubcribeButton/index';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>sky.news</title>
+        <title>Home | sky.news</title>
       </Head>
 
-      <h1>Hellow Dev</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News abaut the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publication <br />
+            <span>for $9.90 month </span>
+          </p>
+
+          <SubscribButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl code" />
+      </main>
     </>
   )
 }
